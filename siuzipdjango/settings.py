da5bfbfed2ae704c2739d848e0f412dfc422f546
hoya6512 <hoya6512@naver.com>
2025-08-14 11:46:25 +0900

    logo static추가 sentry추가

--- siuzipdjango/settings.py
+++ siuzipdjango/settings.py
@@ -259,3 +259,18 @@ SUMMERNOTE_CONFIG = {
         ],
     }
 }
+
+SENTRY_DSN = env.str("SENTRY_DSN", default="")
+if SENTRY_DSN:
+    import sentry_sdk
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=env.float("SENTRY_TRACES_SAMPLE_RATE", default=1.0),
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=env.float("SENTRY_PROFILES_SAMPLE_RATE", default=1.0),
+    )
